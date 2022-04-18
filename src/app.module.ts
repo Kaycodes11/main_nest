65a225d1a26b2dee468552ae6a0ed79ede0a3cc7
KayCodes11 <kayydraws@gmail.com>
2022-04-18 19:55:13 +0530

    websocket  implmented

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
 import appConfig from './config/app.config';
 // import { DatabaseModule } from './database/database.module';
 import { CommonModule } from './common/common.module';
+import { ChatGateway } from 'chat.gateway';
 
 @Module({
   imports: [
@@ -39,6 +40,7 @@ import { CommonModule } from './common/common.module';
   providers: [
     AppService,
     { provide: APP_INTERCEPTOR, useClass: CacheInterceptor },
+    ChatGateway,
   ],
 })
 export class AppModule {}
