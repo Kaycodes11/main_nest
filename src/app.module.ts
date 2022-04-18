a52df028d6e2431cf80b476de66d8eed78d6ce76
KayCodes11 <kayydraws@gmail.com>
2022-04-18 15:19:57 +0530

    Some apis & different ways to dependency injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+// import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'main',
-      autoLoadEntities: true,
-      synchronize: true,
+    // ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ load: [appConfig] }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     CoffeesModule,
+    CoffeeRatingModule,
+    // DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,14 +1,72 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { async } from 'rxjs';
 import { Event } from 'src/events/entities/event.entity';
+import { Connection } from 'typeorm';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import coffeesConfig from './config/coffees.config';
 import { Coffee } from './entity/coffee.entity';
 import { Flavors } from './entity/flavors.entity';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class ConfigService {}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class MockConfigForeDevelopment {}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class MockConfigForProduction {}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    // do something
+    return ['RAGE', 'NESCAGE'];
+  }
+}
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee, Flavors, Event])],
+  imports: [
+    TypeOrmModule.forFeature([Coffee, Flavors, Event]),
+    ConfigModule.forFeature(coffeesConfig),
+  ],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    CoffeeBrandsFactory,
+    // { provide: COFFEE_BRANDS, useValue: ['rage', 'nescage'] },
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create() || ['Rage', 'Nescafe'],
+      inject: [CoffeeBrandsFactory],
+    },
+    {
+      provide: 'DATABASE_CONNECTION',
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        const coffeeBrands = await Promise.resolve(['HomeBrew', 'Nescafe']);
+        console.log(`async factory`);
+        return coffeeBrands;
+      },
+      inject: [Connection],
+    },
+    // {
+    //   provide: COFFEE_BRANDS,
+    //   useClass:
+    //     process.env.NODE_ENV === 'development'
+    //       ? MockConfigForeDevelopment
+    //       : MockConfigForProduction,
+    // },
+  ],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
+
+/*
+custom providers by using useValue, useClass, useFactory, useExisting
+
+coffees.service.ts
+@Inject('COFFEE_BRANDS') private readonly coffeeBrands: string[]
+
+*/
