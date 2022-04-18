fd41d2f9328941f9b1891559dc1c4ef4dbd1bb01
KayCodes11 <kayydraws@gmail.com>
2022-04-18 15:19:57 +0530

    setup db with typeorm done alongside some apis

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,7 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService, ConfigType } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
-import { Repository } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
+import { COFFEE_BRANDS } from './coffees.constants';
+import coffeesConfig from './config/coffees.config';
 import { PaginationQueryDto } from './dto/pagination-query.dto';
 import { Coffee } from './entity/coffee.entity';
 import { Flavors } from './entity/flavors.entity';
@@ -9,11 +12,29 @@ import { Flavors } from './entity/flavors.entity';
 @Injectable()
 export class CoffeesService {
   constructor(
-    @InjectRepository(Coffee) private readonly coffeeRepo: Repository<Coffee>,
+    @InjectRepository(Coffee)
+    private readonly coffeeRepo: Repository<Coffee>,
     @InjectRepository(Flavors)
     private readonly flavorsRepo: Repository<Flavors>,
+    private readonly connection: Connection,
+    // @Inject(coffeesConfig.KEY) private readonly coffeesConfiguration: ConfigType<typeof coffeesConfig>,
     @InjectRepository(Event) private readonly eventRepo: Repository<Event>,
-  ) {}
+    @Inject(COFFEE_BRANDS) private readonly coffeeBrands: string[],
+    @Inject('DATABASE_CONNECTION') private readonly dbConnection: string[],
+    private readonly configService: ConfigService,
+  ) {
+    // console.log(coffeesConfiguration.foo);
+    // const dbHost = this.configService.get<string>('DB_HOST', 'localhost');
+    const dbHost = this.configService.get<string>(
+      'database.DB_HOST',
+      'localhost',
+    ); // when using custom config for env then need to import the configModule to its Module, e.g Coffees.Module.ts
+    const coffeesConfig = this.configService.get<string>('coffees'); // this.configService.get('coffees.foo')
+    console.log('coffees: ', coffeesConfig);
+    console.log(`dbName: `, dbHost);
+    console.log(`HERE: `, coffeeBrands);
+    console.log('DB: ', dbConnection);
+  }
 
   findAll(paginationQuery: PaginationQueryDto) {
     const { limit, offset } = paginationQuery;
